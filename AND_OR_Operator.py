45bc6f005a84c18ac224376d0a1bdc03dad11984
js0554146 <js0554146@gmail.com>
2025-04-24 20:13:13 -0400

    AND_OR_Operator

---
+++ AND_OR_Operator.py
@@ -0,0 +1,15 @@
+a = 7
+b = -1
+c = -19
+
+if a > 0 or b > 0:
+    print("Either of the numbers is greater than 0")
+
+else:
+    print("No number is greater than 0")
+
+if b > 0 or c > 0:
+    print("Either of the numbers is greater than 0")
+
+else:
+    print("No number is greater than 0")
